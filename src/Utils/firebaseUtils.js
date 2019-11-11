ee1ab165550a3cb1a6d9424a1dc8df249af168c8
Ryan Lima <ryanlima08021997@gmail.com>
2019-11-11 01:13:52 -0300

    firebase

--- src/Utils/firebaseUtils.js
+++ src/Utils/firebaseUtils.js
@@ -2,6 +2,8 @@ import firebaseApp from "firebase/app";
 import "firebase/database";
 
 
-const config = {}
+const config = {
+
+}
 export const firebaseImpl = firebaseApp.initializeApp(config)
 export const firebaseDatabase = firebaseApp.database()
